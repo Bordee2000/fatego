5ad8fd061cab88aec026d038d862749c34527223
Bordee Jakteerangkul <51290396+Bordee2000@users.noreply.github.com>
2021-04-27 21:44:49 +0700

    update front

--- frontend/src/views/servant/servantDetail.vue
+++ frontend/src/views/servant/servantDetail.vue
@@ -6,6 +6,169 @@
     <div class="columns is-centered">
       <h1 class="is-size-1 title px-6">Servant Name</h1>
     </div>
+    <!-- step image -->
+    <div class="columns is-centered">
+      <div class="is-multiline is-half has-text-centered" style="width: 25vw">
+        <button class="button is-black is-inverted is-rounded p-2 m-1" disabled>step 1</button>
+        <button class="button is-black is-inverted is-rounded p-2 m-1">step 2</button>
+        <button class="button is-black is-inverted is-rounded p-2 m-1">step 3</button>
+        <button class="button is-black is-inverted is-rounded p-2 m-1">step 4</button>
+        <button class="button is-black is-inverted is-rounded p-2 m-1">step 5</button>
+        <button class="button is-black is-inverted is-rounded p-2 m-1">step 6</button>
+        <button class="button is-black is-inverted is-rounded p-2 m-1">step 7</button>
+        <button class="button is-black is-inverted is-rounded p-2 m-1">step 8</button>
+      </div>
+    </div>
+
+    <!-- detail -->
+    <div class="columns mx-2 mb-6">
+      <!-- left -->
+      <div class="column is-3">
+        <div class="columns">
+          <div
+            class="p-2 textarea"
+            rows="4"
+            style="border: 1px solid black;width: 20vw;box-shadow: 0px 0px 10px;"
+          >
+            <p>Name:</p>
+            <p>AKA:</p>
+            <p>ID:</p>
+            <p>Cost:</p>
+          </div>
+        </div>
+
+        <div class="columns mt-2">
+          <div
+            class="p-2 textarea"
+            rows="3"
+            style="border: 1px solid black;width: 20vw;box-shadow: 0px 0px 10px;"
+          >
+            <p>
+              ATK:
+              <span style="padding-right: 35%;"></span> HP:
+            </p>
+            <p>
+              4★ATK:
+              <span style="padding-right: 27%;"></span>4★HP:
+            </p>
+            <p>
+              Grail ARK:
+              <span style="padding-right: 23%;"></span> Grail HP:
+            </p>
+          </div>
+        </div>
+
+        <div class="columns mt-2">
+          <div
+            class="p-2 textarea"
+            rows="3"
+            style="border: 1px solid black;width: 20vw;box-shadow: 0px 0px 10px;"
+          >
+            <p>Gender:</p>
+            <p>Traits:</p>
+            <p>Alignments:</p>
+          </div>
+        </div>
+
+        <div class="columns is-centered is-multiline has-text-centered mt-5">
+          <div
+            class="card m-2 py-5 is-size-4 has-text-weight-bold"
+            style="width: 30%;box-shadow: 0px 0px 10px;"
+          >QUICK</div>
+          <div
+            class="card m-2 py-5 is-size-4 has-text-weight-bold"
+            style="width: 30%;box-shadow: 0px 0px 10px;"
+          >ARTS</div>
+          <div
+            class="card m-2 py-5 is-size-4 has-text-weight-bold"
+            style="width: 30%;box-shadow: 0px 0px 10px;"
+          >ARTS</div>
+          <div
+            class="card m-2 py-5 is-size-4 has-text-weight-bold"
+            style="width: 30%;box-shadow: 0px 0px 10px;"
+          >BUSTER</div>
+          <div
+            class="card m-2 py-5 is-size-4 has-text-weight-bold"
+            style="width: 30%;box-shadow: 0px 0px 10px;"
+          >BUSTER</div>
+        </div>
+      </div>
+
+      <!-- center image -->
+      <div class="column card is-5 mx-6"></div>
+
+      <!-- right -->
+      <div class="column is-3">
+        <div class="columns">
+          <div
+            class="p-2 textarea" rows="4"
+            style="border: 1px solid black;width: 20vw;box-shadow: 0px 0px 10px;"
+          >
+            <p>Voice Actor:</p>
+            <p>Attribute:</p>
+            <p>Illustrator:</p>
+            <p>Growth Curve::</p>
+          </div>
+        </div>
+
+        <div class="columns mt-2">
+          <div
+            class="p-2 textarea" rows="5"
+            style="border: 1px solid black;width: 20vw;box-shadow: 0px 0px 10px;"
+          >
+            <p>Star Absorption:</p>
+            <p>Star Genteration:</p>
+            <p>NP Change ATK:</p>
+            <p>NP Change DEF:</p>
+            <p>Death Rate:</p>
+          </div>
+        </div>
+
+        <div class="columns is-centered is-multiline has-text-centered mt-5">
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Active Skill</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Passive Skills</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Noble Phantasm</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Ascension</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Skill Upgrade</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >State</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Bond Level</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Biography</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Trivia</button>
+          <Button
+            class="card button m-3 py-2 is-size-4 has-text-weight-bold"
+            style="width: 35%;box-shadow: 0px 0px 10px;"
+          >Images</button>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ frontend/src/views/team.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container is-fluid mt-6">
+    <div class="columns is-centered">
+      <h1 class="is-size-1 title px-6">Team Recommend</h1>
+    </div>
+    <!-- step image -->
+    <div class="columns is-centered">
+      <div class="is-multiline is-half has-text-centered">
+        <button class="button is-black p-5 m-5" disabled>Buster</button>
+        <button class="button is-black is-inverted p-5 m-5">Arks</button>
+        <button class="button is-black is-inverted p-5 m-5">Quick</button>
+      </div>
+    </div>
+
+    <div class="columns is-centered" >
+      <div class="card mr-1" v-for="item in items" :key="item">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+          </figure>
+        </div>
+        <div class="card-header-title">
+          <p>Servant Name</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns is-centered mt-6">
+        <div class="is-max-desktop mx-6 px-6 py-2" style="border: 1px solid black">
+            <div class="column ">
+                <h2>Team 1</h2>
+                <h2>Team 2</h2>
+                <h2>Team 3</h2>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [1,2,3,4,5,6,7],
+    };
+  }
+};
+</script>
